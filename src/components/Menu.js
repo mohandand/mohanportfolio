4569dc723b52cf18fd99389092a7260b0c2acf1d
mohandand <mohandand@gmail.com>
2022-01-14 12:08:41 -0800

    updated code

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import { NavBar} from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 function Menu(){
     return(
         <div>
-            Hello
+           <Navbar bg="myRed" variant="dark" sticky="top" expand="lg" collapseOnSelect>
+               <Navbar.Brand >
+                   MohanDandigam
+               </Navbar.Brand>
+               <Navbar.Toggle/>
+                <Navbar.Collapse>
+                  <Nav className="menu">
+                    {/* <NavDropdown title="Products">
+                            <NavDropdown.Item href="">Tea</NavDropdown.Item>
+                            <NavDropdown.Item href="">Cofee</NavDropdown.Item>
+                    </NavDropdown> */}  
+                    <Nav.Link href="">Home</Nav.Link>
+                    <Nav.Link href="">Resume</Nav.Link>
+                     <Nav.Link href="">Works</Nav.Link>
+                    <Nav.Link href="">Contact</Nav.Link>
+                   </Nav>
+                </Navbar.Collapse>
+           </Navbar>
         </div>
     )
 }
