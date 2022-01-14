60c056cd0131ca32c383abd34412a8c77685f306
mohandand <mohandand@gmail.com>
2022-01-14 11:12:42 -0800

    updated code

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Menu from "./components/Menu.js"
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { NavBar} from 'react-bootstrap'
+
+function Menu(){
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Menu;
